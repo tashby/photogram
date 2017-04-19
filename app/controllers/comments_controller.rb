76d32036ff4fac5b82f6d9d0f7c3f8af96c2e4f9
Tashby <tashby2714@gmail.com>
2017-04-19 04:38:52 -0400

    add partial for comments and jquery to update comments realtime

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,12 +7,14 @@ def create
   @comment.user_id = current_user.id
 
   if @comment.save
-    flash[:success] = "You commented the hell out of that post!"
-    redirect_to :back
+    respond_to do |format|
+      format.html { redirect_to root_path }
+      format.js
+    end
   else
-    flash[:alert] = "Check the comment form, something went horribly wrong."
+    flash[:alert] = "Check the comment form, something went wrong"
     render root_path
-end
+  end
 end
 
 def destroy
